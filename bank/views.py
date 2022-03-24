f95498fba0c360d81ae5807fa9179048879dbcee
Aidin Tvf <at.aidin@gmail.com>
2022-03-25 04:25:26 +0430

    Add customer detail and list view.

--- bank/views.py
+++ bank/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from bank.models import BankAccount, BankCustomer
+from bank.serializers import AccountSerializer, CustomerSerializer
 
-# Create your views here.
+
+class CustomerListView(APIView):
+
+    def get(self, request, *args, **kwargs):
+
+        customers = BankCustomer.objects.all()
+        serialized = CustomerSerializer(customers, many=True)
+        return Response(serialized.data)
+
+    def post(self, request, *args, **kwargs):
+
+        serialized = CustomerSerializer(data=request.data)
+
+        if serialized.is_valid():
+
+            serialized.save()
+            return Response(serialized.data, status.HTTP_201_CREATED)
+
+        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomerDetailView(APIView):
+
+    @staticmethod
+    def get_customer(pk):
+
+        try:
+            customer = BankCustomer.objects.get(pk=pk)
+            return customer
+        except BankCustomer.DoesNotExist:
+            raise Http404
+
+    def get(self, request, *args, **kwargs):
+
+        customer = self.get_customer(kwargs["pk"])
+        serialized = CustomerSerializer(customer)
+
+        return Response(serialized.data)
+
+    def put(self, request, *args, **kwargs):
+
+        customer = self.get_customer(kwargs["pk"])
+        serialized = CustomerSerializer(customer, data=request.data)
+
+        if serialized.is_valid():
+
+            serialized.save()
+            return Response(serialized.data)
+
+        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+
+        customer = self.get_customer(kwargs["pk"])
+        customer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
