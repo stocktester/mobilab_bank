9bcac8e2b0f34791e3e655d152c2861c6f2bfc15
Aidin Tvf <at.aidin@gmail.com>
2022-03-25 05:21:27 +0430

    Add some test for customer list view. Adding namespace to urls file.

--- bank/tests.py
+++ bank/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from django.shortcuts import reverse
+from .views import CustomerListView
 
-# Create your tests here.
+
+class CustomerListViewTests(TestCase):
+
+    def setUp(self) -> None:
+
+        self.factory = APIRequestFactory()
+
+    def test_getting_list(self) -> None:
+
+        request = self.factory.get(reverse("bank:customer"))
+        response = CustomerListView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_making_new_customer(self) -> None:
+
+        new_user = dict(
+            name="Test Test",
+            phone="9891123",
+            email="test@test.test"
+        )
+
+        request = self.factory.post(reverse("bank:customer"), new_user)
+        response = CustomerListView.as_view()(request)
+        self.assertEqual(response.status_code, 201)
+
+    def test_valid_phone(self) -> None:
+
+        new_user = dict(
+            name="Test Test",
+            phone="Test",
+            email="test@test.test"
+        )
+
+        request = self.factory.post(reverse("bank:customer"), new_user)
+        response = CustomerListView.as_view()(request)
+        self.assertEqual(response.status_code, 400)
+
+    def test_valid_email(self) -> None:
+
+        new_user = dict(
+            name="Test Test",
+            phone="9891123",
+            email="testtesttest"
+        )
+
+        request = self.factory.post(reverse("bank:customer"), new_user)
+        response = CustomerListView.as_view()(request)
+        self.assertEqual(response.status_code, 400)
+
+
+
+
+
+
+
+
+
--- bank/urls.py
+++ bank/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "bank"
+
 urlpatterns = [
     path('customer/', views.CustomerListView.as_view(), name='customer'),
     path('customer/<int:pk>', views.CustomerDetailView.as_view(), name='customer_detail'),
