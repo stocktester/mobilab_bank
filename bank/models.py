00f2388ce993a2acfd87a3a7e645612c82fd191b
Aidin Tvf <at.aidin@gmail.com>
2022-04-01 03:18:17 +0430

    Remove balance from Account class.
    Right now, I would like to take the evident approach, which is cumulative
    balance (sum of all transactions.) It might change in future, but right now
    it seems the most practical and bug-free approach. Also migration included.

--- bank/models.py
+++ bank/models.py
@@ -13,10 +13,9 @@ class BankCustomer(models.Model):
 
 class BankAccount(models.Model):
 
-    owner = models.ForeignKey(BankCustomer, on_delete=models.CASCADE)
+    owner = models.ForeignKey(BankCustomer, on_delete=models.CASCADE, related_name='accounts')
     account_name = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     currency = models.CharField(max_length=100, choices=CURRENCY, default="EUR")
-    balance = models.FloatField(default=0)
     closed = models.DateTimeField(null=True)
