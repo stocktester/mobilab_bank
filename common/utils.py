555a7026dd9fe6015bc9594f903814323f83bb5f
Aidin Tvf <at.aidin@gmail.com>
2022-04-01 03:22:40 +0430

    Create UrlLisCreateAPIView based on ListCreateAPIView
    This class is used to pass request to serializer instances.

--- common/utils.py
+++ common/utils.py
@@ -1,3 +1,19 @@
+from rest_framework.generics import ListCreateAPIView
+from rest_framework import status
+from rest_framework.response import Response
+
+
+class UrlLisCreateAPIView(ListCreateAPIView):
+
+    def create(self, request, *args, **kwargs):
+
+        serializer = self.get_serializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
 class ReverseDict(dict):
 
     def __setitem__(self, key, value):
