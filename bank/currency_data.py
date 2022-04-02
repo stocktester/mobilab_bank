01c3580acd7caab292fdea3a746ef4713f151682
Aidin Tvf <at.aidin@gmail.com>
2022-04-02 16:50:40 +0430

    Move ChoiceDict from utils.py to currency_data.py in order to solve circular import problem once and for all

--- bank/currency_data.py
+++ bank/currency_data.py
@@ -1,4 +1,32 @@
-from .utils import ChoiceDict
+class ReverseDict(dict):
+
+    def __setitem__(self, key, value):
+
+        if key in self:
+            del self[key]
+        if value in self:
+            del self[value]
+
+        dict.__setitem__(self, key, value)
+        dict.__setitem__(self, value, key)
+
+    def __delitem__(self, key):
+
+        dict.__delitem__(self, self[key])
+        dict.__delitem__(self, key)
+
+    def __len__(self):
+
+        return dict.__len__(self) // 2
+
+
+class ChoiceDict(ReverseDict):
+
+    def __init__(self, choice_tuple: tuple = None):
+
+        holder_dict = {x[0]: x[1] for x in choice_tuple}
+        super().__init__(**holder_dict)
+
 
 CURRENCY = (('AED', 'United Arab Emirates Dirham'),
             ('AFN', 'Afghan Afghani'),
