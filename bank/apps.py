cd1da33c0706b1cf11c04ae7f3e979ed2821f2e3
Aidin Tvf <at.aidin@gmail.com>
2022-04-04 01:00:44 +0430

    Change populating strategy

--- bank/apps.py
+++ bank/apps.py
@@ -1,34 +1,6 @@
 from django.apps import AppConfig
-from django.conf import settings
-from .populate_db import populate
-from os import environ
 
 
 class BankConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bank'
-
-    def ready(self):
-
-        try:
-            with open(".populated", "r") as pop_file:
-                populated = pop_file.read()
-        except FileNotFoundError as e:
-            populated = False
-
-        if not populated:
-
-            flush = environ.get("FLUSH_DB", False)
-            populate_customer = environ.get("POP_CUSTOMER", False)
-            populate_account = environ.get("POP_ACCOUNT", False)
-            populate_transaction = environ.get("POP_TRANS", False)
-
-            settings.BANK["FLUSH"] = flush
-            settings.BANK["POPULATE"] = dict(
-                customer=populate_customer,
-                account=populate_account,
-                transaction=populate_transaction
-            )
-
-        if settings.BANK.get("POPULATE", None):
-            populate()
