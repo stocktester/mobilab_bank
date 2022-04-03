ef8610ec7884a5cf01ea74bbcf763e004b27fb63
Aidin Tvf <at.aidin@gmail.com>
2022-04-04 01:13:12 +0430

    Using fixture file to populate database.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python manage.py migrate && python manage.py runserver 0.0.0.0:$SERVER_PORT --noreload
+python manage.py migrate && python manage.py flush --noinput && python manage.py loaddata "initial_data.json" &&
+python manage.py runserver 0.0.0.0:$SERVER_PORT --noreload
