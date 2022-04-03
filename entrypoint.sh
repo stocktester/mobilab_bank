eac846ec723dabeae3e1d2f00d5e82503f6d50a2
Aidin Tvf <at.aidin@gmail.com>
2022-04-03 17:59:00 +0430

    Add healthcheck instead of wait_for_postgress.py
    Also use manage.py runserver instead of gunicorn. Handling static files without
    nginx needs some tricks.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python wait_for_postgres.py && python manage.py migrate && python manage.py runserver 0.0.0.0:$SERVER_PORT
+python manage.py migrate && python manage.py runserver 0.0.0.0:$SERVER_PORT
--- gunicorn.conf.py
+++
@@ -1,9 +0,0 @@
-import os
-port = os.environ.get("SERVER_PORT", 8000)
-bind = [f"0.0.0.0:{port}"]
-wsgi_app = "Mobilab_Bank.wsgi:application"
-accesslog = "server.access.log"
-errorlog = "server.error.log"
-pidfile = "gunicorn.pid"
-
-
