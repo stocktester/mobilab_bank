e401f97bcb6d4771ae5eeafe384a654ed7eadac5
Aidin Tvf <at.aidin@gmail.com>
2022-04-04 02:57:12 +0430

    Now populates and flushes only when POPULATE_DB variable is set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-python manage.py migrate && python manage.py flush --noinput && python manage.py loaddata "initial_data.json" &&
-python manage.py runserver 0.0.0.0:$SERVER_PORT --noreload
+python manage.py migrate &&
+(if [ -z "$POPULATE_DB" ]; then echo "Skipping db population."; else
+echo "Flushing database" &&
+python manage.py flush --noinput &&
+echo "Populating Database" &&
+python manage.py loaddata "initial_data.json";fi) &&
+python manage.py test bank &&
+python manage.py runserver 0.0.0.0:"$SERVER_PORT" --noreload
